5ecf5d208d869ed3fc3a8c7a55b6c8b2d6e9ce35
SagiDr <sagi16121960@gmail.com>
2024-01-24 09:35:32 +0200

    Add project files.

---
+++ LoginPage/Model/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginPage.Model
+{
+    internal class User
+    {
+
+        public string Name { get; set; }
+
+        public string password { get; set; }
+
+
+
+    }
+}
